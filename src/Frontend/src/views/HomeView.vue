c90eafc76f580b20bb29813d70adb8bf74f4b062
Anlaharpanda2 <Anlaharpanda2@gmail.com>
2025-04-10 09:57:59 +0700

    10.4.25

--- src/Frontend/src/views/HomeView.vue
+++ src/Frontend/src/views/HomeView.vue
@@ -3,6 +3,8 @@
     <div>
       <h1>Halaman Home</h1>
       <router-link to="/Pendaftaran">Pendaftaran</router-link>
+      <br>
+      <router-link to="/login">login</router-link>
       <p>Selamat Datang</p>
     </div>
   </template>

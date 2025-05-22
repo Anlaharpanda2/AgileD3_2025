5c0c13099bbb8145fdbb34025c73e108733f78a8
NadyaHermaliaPutri <nadyahermaliaputri18@gmail.com>
2025-05-22 08:57:13 +0700

    fix

--- src/Backend/app/Http/Controllers/NadyaController.php
+++ src/Backend/app/Http/Controllers/NadyaController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\pendaftar;
+use App\Models\Pendaftar;
 use Illuminate\Http\Request;
 
  class NadyaController extends Controller
  {
      public function index()
      {
-         return pendaftar::all();
+         return Pendaftar::all();
  }
 }

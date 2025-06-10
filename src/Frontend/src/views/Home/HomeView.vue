6fbf99eeafbac16e3e7f62cffb87d112536fbcc9
Anlaharpanda2 <anlaharpanda2@gmail.com>
2025-06-10 22:04:46 +0700

    fix:home

--- src/Frontend/src/views/Home/HomeView.vue
+++ src/Frontend/src/views/Home/HomeView.vue
@@ -2,26 +2,31 @@
     <DefaultLayout>
         <Slider></Slider>
         <div class="text-center p-3 fw-bold text-secondary">LAYANAN</div>
+        <!-- ini conth nya aja -->
+        <div class="col-1 mx-auto">
+          <el-button type="primary" v-if="isOperator">Operator</el-button>
+          <el-button type="primary" v-if="isPegawai">Pegawai</el-button>
+          <el-button type="primary" v-if="isMasyarakat">Masyarakat</el-button>
+          <el-button type="primary" v-if="isNotLogin">kalau ndak login</el-button>
+        </div>
+
         <div class="d-flex flex-md-row flex-column">
-            <button v-if="isOperator">operator</button>
-            <button v-if="isPegawai">Pegawai</button>
-            <button v-if="isNotLogin">tidak login</button>
-            <button v-if="isMasyarakat">masyarakat</button>
-            <Layanan1 v-if="isOperator"></Layanan1>
-            <Layanan1 v-if="isPegawai"></Layanan1>
-            <Layanan2 v-if="isNotLogin"></Layanan2>
+            <Layanan1></Layanan1>
+            <Layanan2></Layanan2>
+            <Layanan3></Layanan3>
         </div>
     </DefaultLayout>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import DefaultLayout from '../../layouts/DefaultLayout.vue';
 import Slider from '../../components/Home/Slider.vue';
 import Layanan1 from '../../components/Home/Layanan1.vue';
 import Layanan2 from '../../components/Home/Layanan2.vue';
 import Layanan3 from '../../components/Home/Layanan3.vue';
+import { Delete, Edit, Search, Share, Upload } from '@element-plus/icons-vue'
 
-import { computed } from 'vue'
 
 function getUserRole() {
   return localStorage.getItem('role') || ''

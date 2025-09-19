5cb09a74269c448915b3b713f5fedaa241a5b7ae
Anla Harpanda <anlaharpanda2@gmail.com>
2025-09-19 21:42:03 +0700

    fix:vercel deploy10

--- src/Backend/api/index.php
+++ src/Backend/api/index.php
@@ -1,4 +1,20 @@
 <?php
 
-// Forward Vercel requests to normal index.php
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
+
+// 
+// INI BAGIAN PENTINGNYA!
+// Memperbaiki path request dari Vercel agar bisa dibaca oleh Router Laravel.
+//
+if (isset($_SERVER['REQUEST_URI'])) {
+    $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
+}
+
+// Baris ini akan memanggil file index.php utama milik Laravel
+// yang ada di dalam folder public.
 require __DIR__ . '/../public/index.php';
